7c6b6ce90151efd5af18a32e2db8c1d38c029ff8
KourosBey <38757771+KourosBey@users.noreply.github.com>
2022-08-11 18:11:23 +0300

    GoogleSignInAdded
    Google SignIn methodu eklendi.

--- kosuprogrami/lib/services/auth_service.dart
+++ kosuprogrami/lib/services/auth_service.dart
@@ -2,14 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kosuprogrami/login/loginView.dart';
 import 'package:kosuprogrami/login/registerView.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService {
+class AuthService extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
   handleAuthState() {
     return StreamBuilder(
       stream: FirebaseAuth.instance.authStateChanges(),
@@ -61,5 +64,21 @@ class AuthService {
       }
     }
     return user!.user;
+  }
+
+  Future<void> googleLogin() async {
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser == null) return;
+    _user = googleUser;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    notifyListeners();
   }
 }
