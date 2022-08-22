5955664f85afc83cdc9b00e3768bc57e06a6d5d3
KourosBey <38757771+KourosBey@users.noreply.github.com>
2022-08-22 07:19:30 +0300

    GoogleMaps-CurrentLocationTracking

--- kosuprogrami/lib/screens/dashboard/activity/newActivity.dart
+++ kosuprogrami/lib/screens/dashboard/activity/newActivity.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class NewActivy extends StatefulWidget {
   const NewActivy({Key? key}) : super(key: key);
@@ -8,6 +11,23 @@ class NewActivy extends StatefulWidget {
 }
 
 class _NewActivyState extends State<NewActivy> {
+  final LatLng _initialcameraposition = const LatLng(20.5937, 78.9629);
+  late GoogleMapController _controller;
+  final Location _location = Location();
+
+  LocationData? currentLocation;
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+    _location.onLocationChanged.listen((l) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!), zoom: 20),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +35,9 @@ class _NewActivyState extends State<NewActivy> {
         backgroundColor: Colors.orange,
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Padding(padding: EdgeInsets.all(5.0)),
             routeMenuWidget(context),
             Center(
               child: Container(
@@ -29,7 +48,221 @@ class _NewActivyState extends State<NewActivy> {
                     child: const Icon(Icons.play_circle_sharp, size: 60)),
               ),
             ),
+            statusForExercise(),
           ]),
+    );
+  }
+
+  Center statusForExercise() {
+    return Center(
+      child: Container(
+          decoration: const BoxDecoration(color: Colors.grey),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "5.2KM",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "yürüdüğünüz yol",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 
@@ -39,8 +272,56 @@ class _NewActivyState extends State<NewActivy> {
         width: MediaQuery.of(context).size.width,
         height: 300,
         decoration: const BoxDecoration(color: Colors.grey),
-        child: Container(),
+        child: Container(
+          child: _buildGoogleMap(context),
+        ),
       ),
     );
   }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _initialcameraposition),
+        mapType: MapType.normal,
+        onMapCreated: _onMapCreated,
+        myLocationEnabled: true,
+        zoomControlsEnabled: true,
+      ),
+    );
+  }
+
+  // LocationData? currentLocation;
+  // void stateSet() async {
+  //   Position position = await _determinePosition();
+  //   setState(() {
+  //     googleMapController?.animateCamera(CameraUpdate.newCameraPosition(
+  //         CameraPosition(
+  //             target: LatLng(position.latitude, position.latitude), zoom: 20)));
+  //   });
+  // }
+
+  // Future<Position> _determinePosition() async {
+  //   bool serviceEnabled;
+  //   LocationPermission permission;
+
+  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  //   if (!serviceEnabled) {
+  //     return Future.error("Location services are disabled");
+  //   }
+  //   permission = await Geolocator.checkPermission();
+  //   if (permission == LocationPermission.denied) {
+  //     permission = await Geolocator.requestPermission();
+  //     if (permission == LocationPermission.denied) {
+  //       return Future.error("Location Permission Denied");
+  //     }
+  //   }
+  //   if (permission == LocationPermission.deniedForever) {
+  //     return Future.error("Permission Denied Forever");
+  //   }
+  //   Position position = await Geolocator.getCurrentPosition();
+  //   return position;
+  // }
 }
